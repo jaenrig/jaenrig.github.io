3ae0bbb660f4c607fe587740c48133115cdb63ee
Enriquez Garcia Juan Antonio (IFAG DES TCP EDS) <JuanAntonio.EnriquezGarcia@infineon.com>
2019-08-08 14:44:57 +0200

    Added new classes

--- src/lib.h
+++ src/lib.h
@@ -12,4 +12,27 @@ class library
         void functionA();
         void functionB(int arg);
         int  functionC(int& arg);
+        static functionD(){};
+};
+
+class something
+{
+    private:
+        bool flag;
+        int  number;
+        int  another_number;
+
+    public:
+        something() {};
+        something(bool flag, int number, int another_number) : flag(flag), number(number), another_number(another_number) {};
+        void product(){ library::functionD(); return (number*another_number); };
+};
+
+class child: public library
+{
+    protected:
+        something objS;
+    
+    public:
+        child(){};
 };
